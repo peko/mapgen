02f5074f172ecbc6ff4490e444cdb52e8481799a
peko <gasu.basu@gmail.com>
2018-09-07 01:39:40 +0300

    up

--- Clipper/clipper_c.cpp
+++ Clipper/clipper_c.cpp
@@ -1,2 +1,90 @@
+#include <string.h>
 
+#include "clipper.h"
+#include "clipper_c.h"
+
+using namespace clipperlib;
+
+void kpathsToPaths(kpaths_t* kpaths, Paths& paths);
+void pathsToKpaths(Paths& paths, kpaths_t* kpaths);
+
+extern "C" void clip(
+    kpaths_t* ksubj,
+    kpaths_t* kclip,
+    kpaths_t* kresult) {
+
+    ClipType clip_type = ctIntersection;
+    FillRule fill_rule = frNonZero;
+    Paths subj, clip, result;
+
+    kpathsToPaths(ksubj,   subj  );
+    kpathsToPaths(kclip,   clip  );
+    kpathsToPaths(kresult, result);
+    
+    Clipper clipper;
+    clipper.AddPaths(subj, ptSubject);
+    clipper.AddPaths(clip, ptClip);
+    clipper.Execute(clip_type, result, fill_rule);
+
+    pathsToKpaths(result, kresult);
+}
+
+void kpathsToPaths(
+    kpaths_t* kpaths, 
+    Paths& paths) {
+
+    paths.resize(kpaths->n);
+    // iterate through paths
+    for(size_t pid=0; pid<kpaths->n; pid++) {
+        // copy every path to vector
+        kpath_t* kpath = &kpaths->a[pid];
+        paths[pid].assign(
+            (Point64*) (kpath->a           ), 
+            (Point64*) (kpath->a + kpath->n));
+    }
+}
+
+void pathsToKpaths(Paths& paths, kpaths_t* kpaths) {
+    if(kpaths->n != 0 || kpaths->a !=0) kpaths_free(kpaths);
+    for(size_t pid=0; pid<paths.size(); pid++) {
+        Path& path = paths[pid];
+        kpath_t* kpath = kpath_new();
+        kv_resize(kpoint_t, *kpath, path.size());
+        memcpy(kpath->a, path.data(), sizeof(kpoint_t)*(kpath->n));
+    }
+}
+
+// kpath - point collection
+
+kpath_t* kpath_new() {
+    kpath_t* kpath = (kpath_t*) calloc(1, sizeof(kpath_t));
+    return kpath;
+}
+
+void kpath_free(kpath_t* kpath) {
+    free(kpath->a);
+}
+
+void kpath_add_point(kpath_t* kpath, kpoint_t* kpoint) {
+    kv_push(kpoint_t, *kpath, *kpoint);
+}
+
+// kpaths - path collection
+
+kpaths_t* kpaths_new() {
+    kpaths_t* kpaths = (kpaths_t*) calloc(1, sizeof(kpaths_t));
+    return kpaths;
+}
+
+void kpaths_free(kpaths_t* kpaths) {
+    for(size_t pid=0; pid<kpaths->n; pid++) {
+        kpath_free(&kpaths->a[pid]);
+    }
+    free(kpaths->a);
+    *kpaths = (kpaths_t) {0,0,0};
+}
+
+void kpaths_add_path(kpaths_t* kpaths, kpath_t* kpath) {
+    kv_push(kpath_t, *kpaths, *kpath);
+}
 
--- Clipper/clipper_c.h
+++ Clipper/clipper_c.h
@@ -2,11 +2,25 @@
 
 #include "kvec.h"
 
-typedef struct {uint64_t x, y;} point_t;
-typedef kvec_type(point_t) path_t;
-typedef kvec_type(path_t) paths_t;
+typedef struct {int64_t x, y;} kpoint_t;
+typedef kvec_t(kpoint_t) kpath_t;
+typedef kvec_t(kpath_t) kpaths_t;
 
-extern "C" clip(
-    paths_t* subj,
-    paths_t* clip,
-    paths_t* result);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void clip(
+       kpaths_t* subj,
+       kpaths_t* clip,
+       kpaths_t* result);
+
+    kpath_t* kpath_new();
+    void kpath_free(kpath_t* kpath);
+    void kpath_add_point(kpath_t* kpath, kpoint_t* kpoint);
+
+    kpaths_t* kpaths_new();
+    void kpaths_free(kpaths_t* kpaths);
+    void kpaths_add_path(kpaths_t* kpaths, kpath_t* kpath);
+#ifdef __cplusplus
+}
+#endif
